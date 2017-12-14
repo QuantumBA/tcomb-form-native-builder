7b6796b9c2c6b26df96e852f63cf3d73df4f33fd
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2017-12-14 09:43:08 +0100

    Improved linting

--- setupTests.js
+++ setupTests.js
@@ -9,7 +9,7 @@ global.navigator = {userAgent: 'node.js'}
 function copyProps(src, target)
 {
   const props = Object.getOwnPropertyNames(src)
-  .filter((prop) => typeof target[prop] === 'undefined')
+  .filter(prop => typeof target[prop] === 'undefined')
   .reduce((result, prop) => ({
     ...result,
     [prop]: Object.getOwnPropertyDescriptor(src, prop)
